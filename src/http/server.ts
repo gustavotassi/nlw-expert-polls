31ca213cf663ad5d096db7fdbf62d96980ab4af5
gustavotassi <souzagustavotassi@gmail.com>
2024-02-10 19:15:13 -0300

    feat: add get poll and vote on poll endpoints

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,19 @@
 import Fastify from 'fastify'
 import { createPoll } from './routes/create-poll'
+import { getPoll } from './routes/get-poll'
+import { voteOnPoll } from './routes/vote-on-poll'
+import cookie from '@fastify/cookie'
 
 const app = Fastify()
 
+app.register(cookie, {
+  secret: 'polls-app-nlw',
+  hook: 'onRequest',
+})
+
+app.register(getPoll)
 app.register(createPoll)
+app.register(voteOnPoll)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running')
