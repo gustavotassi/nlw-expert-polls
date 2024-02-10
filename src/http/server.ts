0abcbe1d279d8387dd9fb5df3a69a2f8c00e710f
gustavotassi <souzagustavotassi@gmail.com>
2024-02-10 14:49:05 -0300

    feat: add new polls POST request to create a new poll

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,28 @@
 import Fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+import { z } from 'zod'
 
 const app = Fastify()
+
+const prisma = new PrismaClient()
+
+app.post('/polls', async (request, reply) => {
+  const createPollBodySchema = z.object({
+    title: z.string(),
+  })
+
+  const { title } = createPollBodySchema.parse(request.body)
+
+  const poll = await prisma.poll.create({
+    data: {
+      title,
+    },
+  })
+
+  return reply.status(201).send({
+    pollId: poll.id,
+  })
+})
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running')
