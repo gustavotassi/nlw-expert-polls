26f5a04edd54bc6f96aff10d730e55a406fba55b
gustavotassi <souzagustavotassi@gmail.com>
2024-02-14 18:20:25 -0300

    feat: add ranking based on votes to poll routes

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,7 +2,9 @@ import Fastify from 'fastify'
 import { createPoll } from './routes/create-poll'
 import { getPoll } from './routes/get-poll'
 import { voteOnPoll } from './routes/vote-on-poll'
+import { pollResults } from './ws/poll-results'
 import cookie from '@fastify/cookie'
+import websocket from '@fastify/websocket'
 
 const app = Fastify()
 
@@ -11,9 +13,13 @@ app.register(cookie, {
   hook: 'onRequest',
 })
 
+app.register(websocket)
+
 app.register(getPoll)
 app.register(createPoll)
 app.register(voteOnPoll)
+
+app.register(pollResults)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running')
