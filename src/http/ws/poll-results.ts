fbb21717b3bfd582a57f67bcc0a4bb17667543ef
gustavotassi <souzagustavotassi@gmail.com>
2024-02-14 18:20:39 -0300

    feat: add websocket route to get poll votes in real time

---
+++ src/http/ws/poll-results.ts
@@ -0,0 +1,21 @@
+import { FastifyInstance } from 'fastify'
+import { voting } from '../../utils/voting-pub-sub'
+import z from 'zod'
+
+export async function pollResults(app: FastifyInstance) {
+  app.get(
+    '/polls/:pollId/results',
+    { websocket: true },
+    (connection, request) => {
+      const getPollParamsSchema = z.object({
+        pollId: z.string().uuid(),
+      })
+
+      const { pollId } = getPollParamsSchema.parse(request.params)
+
+      voting.subscribe(pollId, (message) => {
+        connection.socket.send(JSON.stringify(message))
+      })
+    },
+  )
+}
